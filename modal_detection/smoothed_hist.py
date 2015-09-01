5e9f76e1b05f5f93e495729e0dad444d605deb46
Shirui Lu <lushirui1118+git@gmail.com>
2015-09-01 11:21:18 +0800

    [+] smooth hist using kde & for silverman in R

--- modal_detection/smoothed_hist.py
+++ modal_detection/smoothed_hist.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from kde_for_scipy.core.kde import kde
 from scipy.stats import gaussian_kde
 
+
 class SmoothedHist:
     IMG_DIR = '../resources/images/'
+
     @staticmethod
     def plot_smoothed_hist(data, N=None, MIN=None, MAX=None):
         """
@@ -16,24 +18,32 @@ class SmoothedHist:
         """
         bw, mesh, kdense = kde(data, N, MIN, MAX)
         print bw, mesh, kdense
-        plt.plot(mesh, kdense); plt.title('#bins='+str(N)); plt.show()
+        plt.plot(mesh, kdense)
+        plt.title('#bins=' + str(N))
+        plt.show()
         SmoothedHist.test_scipy_kde(data, bw, mesh)
 
     @staticmethod
     def test_scipy_kde(data, bw, mesh):
-        """ use kde_for_scipy to aid bandwidth selection """
-        kde_sci = gaussian_kde(data, bw)
-        plt.plot(mesh, kde_sci.evaluate(mesh)); plt.title('scipy.stats'); plt.show()
-
+        """ use kde_for_scipy to aid mesh generation """
+        kde_sci = gaussian_kde(data)
+        plt.plot(mesh, kde_sci.evaluate(mesh))
+        plt.title('scipy.stats')
+        plt.show()
 
     @staticmethod
     def main():
-        imgLab = cv2.cvtColor( cv2.imread(SmoothedHist.IMG_DIR+'teaser_face.png'),
-                               cv2.COLOR_BGR2LAB )
+        IMG_NAME = 'teaser_face'
+        imgLab = cv2.cvtColor(
+            cv2.imread(SmoothedHist.IMG_DIR + IMG_NAME + '.png'),
+            cv2.COLOR_BGR2LAB)
         imgLab = imgLab.astype('uint8')
-        # for i in xrange(5,255,5):
-        SmoothedHist.plot_smoothed_hist(imgLab[...,0].ravel())
-
+        SmoothedHist.plot_smoothed_hist(imgLab[..., 0].ravel())
+        # save image data for R silverman analysis
+        # import os
+        # import numpy as np
+        # DATA_DIR = os.path.expanduser('~/Dropbox/dropcode/r/')
+        # np.savetxt(DATA_DIR+IMG_NAME+'.csv', imgLab[...,0].ravel(), delimiter=',')
 
 
 ####################################################
